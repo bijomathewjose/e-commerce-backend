09fc52b5e42aa9a32d45c1d18afebe393b75ccc2
rajasree <rajasree.r@fayausa.com>
2023-03-09 17:49:03 +0530

    update

---
+++ customer/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Customer(models.Model):
+    name=models.CharField(max_length=50,null=False)
+    email=models.EmailField(_(""), max_length=254,null=False)
+    class Meta:
+        db_table='customer'
+
+class Product(models.Model):
+    name=models.CharField(max_length=50,null=False)
+    description=models.TextField(max_length=200)
+    customer_id=models.ForeignKey(Customer,on_delete=models.CASCADE,null=False)
+    is_active=models.BooleanField(default=True)
+    class Meta:
+        db_table='product'
+
