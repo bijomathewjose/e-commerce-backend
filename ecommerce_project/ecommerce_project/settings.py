97d286beeaa8845de944df2012a4b369e2746190
rajasree <rajasree.r@fayausa.com>
2023-03-10 10:32:52 +0530

    update

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -82,15 +82,10 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':os.getenv('DATABASE_ENGINE'),
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER':os.getenv('DATABASE_USER'),
-        'PASSWORD':os.getenv('DATABASE_PASSWORD'),
-        'HOST':os.getenv('DATABASE_HOST'),
-        'PORT':os.getenv('DATABASE_PORT')
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':BASE_DIR/'db.sqlite3'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
